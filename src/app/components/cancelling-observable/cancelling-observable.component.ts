3fec62a4d100187eb316b0d1dc4166aac974c802
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-08 17:57:20 +0530

    9. Avoid Memory leakage when subscribing and unsubscribing Custom Observable by cleaning Code - RxJS

--- src/app/components/cancelling-observable/cancelling-observable.component.ts
+++ src/app/components/cancelling-observable/cancelling-observable.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cancelling-observable',
@@ -8,18 +8,39 @@ import { interval, Subscription } from 'rxjs';
 })
 export class CancellingObservableComponent {
   //What is a Subscription? A Subscription is an object that represents a disposable resource, usually the execution of an Observable. A Subscription has one important method, unsubscribe, that takes no argument and just disposes the resource held by the subscription. In previous versions of RxJS, Subscription was called "Disposable".
-  timerSubscription!: Subscription;
+  timerConsoleSubscription!: Subscription;
+  timerBrowserSubscription!: Subscription;
+  timers:number[] = [];
   constructor() {}
   ngOnInit(): void {
-    // interval: Creates an Observable that emits sequential numbers every specified interval of time
-    const newObservable = interval(1000);
-    this.timerSubscription = newObservable.subscribe((data) => {
-      console.log(data);
+    const newObservable = new Observable<number>((obsever) => {
+      let i = 0;
+      let interval = setInterval(() => {
+        console.log('Interval executing');
+        obsever.next(i++);
+      }, 1000);
+      // the below function will called when observer is unsubscribed and
+      // the below method will execute whenever the error will occured.
+      return () => {
+        console.log('called when observer is unsubscribed');
+        clearInterval(interval);
+      };
     });
+    this.timerConsoleSubscription = newObservable.subscribe((data) => {
+      console.log(new Date().toLocaleDateString() + '' + data);
+    });
+
+    this.timerBrowserSubscription = newObservable.subscribe((data) => {
+      this.timers.push(data);
+    });
+
+    // we can club two subscrtiptions like below
+    this.timerConsoleSubscription.add(this.timerBrowserSubscription)
   }
 
   cancelTimer() {
     console.log('cancel the observer');
-    this.timerSubscription.unsubscribe();
+    this.timerConsoleSubscription.unsubscribe();
+    //this.timerBrowserSubscription.unsubscribe();
   }
 }
