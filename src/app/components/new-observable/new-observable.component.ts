8e97c8314d4b18a0d1ce6e44e78dcac237381fcd
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-08 12:53:44 +0530

    6. Different ways of creating the observer by object & class and subscribing to Observables in RxJS.

--- src/app/components/new-observable/new-observable.component.ts
+++ src/app/components/new-observable/new-observable.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Observer4 } from './services/Observer4';
 
 @Component({
   selector: 'app-new-observable',
@@ -10,20 +11,31 @@ export class NewObservableComponent {
   ngOnInit(): void {
     const newObservable = new Observable<number>((observer) => {
       for (let i = 0; i < 5; i++) {
-        if (i === 4) {
-          observer.error('Unknown error: I value is 4');
-        }
         observer.next(i);
       }
       observer.complete();
     });
 
     let observer = {
-      next: (data: number) => console.log(data),
+      next: (data: number) => console.log('observer 1' + data),
       error: (error: string) => console.log(error),
       complete: () => console.log('all done complete'),
     };
-
     newObservable.subscribe(observer);
+
+    newObservable.subscribe({
+      next: (data: number) => console.log('observer 2' + data),
+      error: (error: string) => console.log(error),
+      complete: () => console.log('all done complete'),
+    });
+
+    newObservable.subscribe(
+      (data) => console.log('onserver 3' + data),
+      (error) => console.log(error),
+      () => console.log('complete Done')
+    );
+
+    newObservable.subscribe(new Observer4());
+
   }
 }
