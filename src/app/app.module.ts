46d0d73f4d0914b1b963f9d6783e24e7fdeba9de
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-14 18:53:11 +0530

    42. RxJS Ajax Operator for making HTTP requests. Learn RxJS Creation Category Ajax Operator - RxJS.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,8 @@ import { ElementAtOperatorComponent } from './pages/element-at-operator/element-
 import { IgnoreElementsOperatorComponent } from './pages/ignore-elements-operator/ignore-elements-operator.component';
 import { SingleOperatorComponent } from './pages/single-operator/single-operator.component';
 import { MapOperatorComponent } from './pages/map-operator/map-operator.component';
+import { MapToOperatorComponent } from './pages/map-to-operator/map-to-operator.component';
+import { AjaxOperatorComponent } from './pages/ajax-operator/ajax-operator.component';
 
 @NgModule({
   declarations: [
@@ -69,6 +71,8 @@ import { MapOperatorComponent } from './pages/map-operator/map-operator.componen
     IgnoreElementsOperatorComponent,
     SingleOperatorComponent,
     MapOperatorComponent,
+    MapToOperatorComponent,
+    AjaxOperatorComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
