a8b3cb4d3d81f544e24e9fb1772f64d0b0f8da11
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-15 01:27:11 +0530

    58. Publish, Multicast, refCount & Share operator. These Converts Cold to Hot Observables:

--- src/app/pages/multicast-operator/multicast-operator.component.ts
+++ src/app/pages/multicast-operator/multicast-operator.component.ts
@@ -29,8 +29,6 @@ export class MulticastOperatorComponent implements OnInit {
     // }, 3000);
     // source.connect();
 
-    // Creates an observable that multicasts once connect() is called on it.
-    // Connectable<T>: A "connectable" observable, that has a connect() method, that you must call to connect the source to all consumers through the subject provided as the connector.
     let source = connectable(interval(1000));
     source.subscribe((data) => {
       console.log(data);
