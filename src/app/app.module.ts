1cb83d96fa1b9c3019cf2bc24e15f575efa058ab
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-14 09:28:43 +0530

    19. RxJS Take Operator. Learn RxJS Filtering Category Take Operator - RxJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { OperatorsComponent } from './pages/operators/operators.component';
 import { BufferOperatorComponent } from './pages/buffer-operator/buffer-operator.component';
 import { BufferCountOperatorComponent } from './pages/buffer-count-operator/buffer-count-operator.component';
 import { BufferTimeOperatorComponent } from './pages/buffer-time-operator/buffer-time-operator.component';
+import { BufferToggleOperatorComponent } from './pages/buffer-toggle-operator/buffer-toggle-operator.component';
+import { BufferWhenOperatorComponent } from './pages/buffer-when-operator/buffer-when-operator.component';
+import { TakeOperatorComponent } from './pages/take-operator/take-operator.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +26,9 @@ import { BufferTimeOperatorComponent } from './pages/buffer-time-operator/buffer
     BufferOperatorComponent,
     BufferCountOperatorComponent,
     BufferTimeOperatorComponent,
+    BufferToggleOperatorComponent,
+    BufferWhenOperatorComponent,
+    TakeOperatorComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
