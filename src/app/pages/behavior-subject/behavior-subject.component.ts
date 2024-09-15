4ae702f9b4c07242fb0a2040b7ddd359d2e30e39
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-15 09:22:49 +0530

    60. Replay Subject | Learn Replay Subject vs Behavior Subject - RxJS.

--- src/app/pages/behavior-subject/behavior-subject.component.ts
+++ src/app/pages/behavior-subject/behavior-subject.component.ts
@@ -15,17 +15,12 @@ export class BehaviorSubjectComponent implements OnInit {
     behaviorSubject$.subscribe((data) => {
       console.log('observer 1' + data);
     });
-    // Here we have a subscriber observer1, before emit the value.
-    // it will recevie the 0 value which we are sending in Behaviour Subject
-    // that means the subscriber is receving the deafault value from the behaviour subject, before actual emit.
 
-    behaviorSubject$.next(1); 
-    // Now the actual emit happend with the value 1, so here the behaviour Subject will hold the previous emitted value in buffer and that will share to the next subscriptions.
+    behaviorSubject$.next(1); //
 
     behaviorSubject$.subscribe((data) => {
       console.log('observer 2' + data);
     });
-    // In above console, we will recive the data from 1 value, which was stored in buffer
 
     behaviorSubject$.next(2);
   }
