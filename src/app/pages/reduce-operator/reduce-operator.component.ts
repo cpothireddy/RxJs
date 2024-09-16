8e8d83fc8ff340ebfa7e0b2cdf266edead03c95c
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-16 22:32:25 +0530

    87. RxJS IsEmpty operator. Learn RxJS Conditional and Boolean IsEmpty Operator - RxJS.

--- src/app/pages/reduce-operator/reduce-operator.component.ts
+++ src/app/pages/reduce-operator/reduce-operator.component.ts
@@ -15,38 +15,26 @@ export class ReduceOperatorComponent implements OnInit {
     source$
       .pipe(
         scan((acc, val) => {
-          console.log('scan accumulator ' + acc);
-          console.log('scan Value ' + val);
+          console.log('accumulator ' + acc);
+          console.log('Value ' + val);
           return acc + val;
-        }, 0)
+        }, 0),
+        last()
       )
       .subscribe((data) => {
-        console.log('scan result ' + data);
+        console.log('result ' + data);
       });
-    // scan will return result for every return but reduce will return the result only once that is last. scan with last operator will eork exactly same like reduce.
-    // source$
-    // .pipe(
-    //   scan((acc, val) => {
-    //     console.log('scan accumulator ' + acc);
-    //     console.log('scan Value ' + val);
-    //     return acc + val;
-    //   }, 0),
-    //last()
-    // )
-    // .subscribe((data) => {
-    //   console.log('scan result ' + data);
-    // });
 
     source$
       .pipe(
         reduce((acc, val) => {
-          console.log('reduce accumulator ' + acc);
-          console.log('reduce Value ' + val);
+          console.log('accumulator ' + acc);
+          console.log('Value ' + val);
           return acc + val;
-        }, 0) // here the default accumulator value is 0, and acc is the accumulator and val is the every emit value from the observable.
+        }, 0)
       )
       .subscribe((data) => {
-        console.log('reduce result ' + data);
+        console.log('result ' + data);
       });
   }
 }
